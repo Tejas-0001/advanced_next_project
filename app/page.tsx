99f76ffab4aa9977e56240752c289d4cb8e46931
Tejas-0001 <202111080@diu.iiitvadodara.ac.in>
2024-03-26 19:12:59 +0530

    Api on server

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import { Main } from "next/document";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <main>Hello World !!</main>
+   <main>
+    <h1>Hello World !!</h1>
+    <Link href="/users">Users</Link>
+   </main>
+   
   );
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const UsersPage = () => {
+interface User{
+  id:number;
+  name:string;  
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users : User[] = await res.json();
   return (
-    <div>UsersPage</div>
+    <>
+    <h1>Users</h1> 
+    <ul>
+      {users.map(item => <li key={item.id}>{item.name}</li>)}
+    </ul>
+    </>
   )
 }
 
