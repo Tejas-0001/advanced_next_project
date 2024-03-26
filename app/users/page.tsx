d85abe8d6ebfcdd1ac5b11af9718a77947b9297e
Tejas-0001 <202111080@diu.iiitvadodara.ac.in>
2024-03-26 19:20:37 +0530

    statica and dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,11 +6,13 @@ interface User{
 }
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  //const res = await fetch('https://jsonplaceholder.typicode.com/users',{next:{revalidate:10}})every 10 seconds it will rerun
+  const res = await fetch('https://jsonplaceholder.typicode.com/users',{cache:'no-store'})//every request render Dynamic rendering,default is cached store so static rendering
   const users : User[] = await res.json();
   return (
     <>
     <h1>Users</h1> 
+    <p>{new Date().toString()}</p>
     <ul>
       {users.map(item => <li key={item.id}>{item.name}</li>)}
     </ul>
